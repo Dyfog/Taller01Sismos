e14dd0a18a96b8a17f9a4a422a5fcbed9e6b2c36
Dyfog <mariolaurie2003@gmail.com>
2023-03-23 10:29:39 -0300

    1.3
    programa ya terminado, inclusión de un método "iniciar" que corre el código completo

--- Main.java
+++ Main.java
@@ -1,49 +1,50 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        iniciar();
     }
-    public static double[] llenarSismos (){
-        double[] arreglo = new double[168];
-        for (int i=0;i<arreglo.length;i++){
-            arreglo[i]= (Math.floor((Math.random()*10)*10))/10;
-            System.out.println(arreglo[i]);
+
+    public static double[] llenarSismos() {
+        double[] sismos = new double[168];
+        for (int i = 0; i < sismos.length; i++) {
+            sismos[i] = (Math.floor((Math.random() * 10) * 10)) / 10;
         }
-        return arreglo;
+        return sismos;
     }
-    public static void leerSismos(double[] sismos){
-        for (double sismo : sismos) {
-            System.out.println(sismo);
-        }
-    }
-    public static void leerMayorSismo(double[] sismos){
+
+
+    public static void leerMayorSismo(double[] sismos) {
         double mayorSismo = 0;
-        for (double sismo : sismos) {
-            if (sismo > mayorSismo) {
-                mayorSismo = sismo;
+        for (int i = 0; i < sismos.length; i++) {
+            if (sismos[i] > mayorSismo) {
+                mayorSismo = sismos[i];
             }
         }
-        System.out.println("El sismo de mayor intensidad fue de: "+mayorSismo);
+        System.out.println(mayorSismo);
     }
-    public static int contarSismosMayor5 (double[] sismos){
+
+
+    public static int contarSismosMayor5(double[] sismos) {
         int cantSismos = 0;
-        for (double sismo : sismos){
-            if (sismo>=5){
+        for (double sismo : sismos) {
+            if (sismo >= 5) {
                 cantSismos++;
             }
         }
         return cantSismos;
     }
-    public static void mandaSms (double[] sismos){
-        for (double sismo : sismos){
-            if (sismo>=7){
+
+
+    public static void mandaSms(double[] sismos) {
+        for (double sismo : sismos) {
+            if (sismo >= 7) {
                 System.out.println("En estos momentos hay un sismo de gran intensidad (mayor o igual a 7), por lo que debe evacuar lo antes posible");
             }
         }
     }
-    public static void menu (){
-        double[] sismos = new double[168];
+
+
+    public static void menu(double[] sismos) {
         Scanner in = new Scanner(System.in);
         System.out.println("Bienvenido, ingrese una de las siguientes opciones:");
         System.out.println("1.-Ingresar datos");
@@ -52,43 +53,57 @@ public class Main {
         System.out.println("4.-Enviar SMS por cada sismo mayor o igual a 7.0");
         System.out.println("5.-Salir del programa");
         int eleccion = in.nextInt();
-        switch (validarRangoNumero(eleccion,1,5)){
+        switch (validarRangoNumero(eleccion, 1, 5)) {
             case 1:
                 sismos = llenarSismos();
                 System.out.println("Datos ingresados exitosamente!");
+                menu(sismos);
                 break;
             case 2:
                 leerMayorSismo(sismos);
+                menu(sismos);
                 break;
             case 3:
-                System.out.println("La cantidad de sismos de intensidad mayor o igual a 5 fueron: "+contarSismosMayor5(sismos));
+                System.out.println("La cantidad de sismos de intensidad mayor o igual a 5 fueron: " + contarSismosMayor5(sismos));
+                menu(sismos);
                 break;
             case 4:
                 mandaSms(sismos);
+                menu(sismos);
                 break;
             case 5:
                 System.out.println("Está seguro?");
                 System.out.println("1.-Si");
                 System.out.println("2.-No");
-                int salir = validarRangoNumero(in.nextInt(),1,2);
-                if (salir==2){
-                    menu();
+                int salir = validarRangoNumero(in.nextInt(), 1, 2);
+                if (salir == 2) {
+                    menu(sismos);
+                } else {
+                    System.out.println("Cerrando programa");
                 }
-
         }
     }
-    public static int validarRangoNumero (int numero,int min,int max){
+
+
+    public static int validarRangoNumero(int numero, int min, int max) {
         Scanner in = new Scanner(System.in);
-        try{
-            while (numero<min || numero>max){
+            while (numero < min || numero > max) {
                 System.out.println("El numero no esta dentro de los parametros, ingrese nuevamente: ");
                 numero = in.nextInt();
             }
-            return numero;
-        }catch (Exception e){
-            System.out.println("el valor ingresado no es valido, ingrese nuevamente");
-            validarRangoNumero(numero,min,max);
-        }
         return numero;
+    }
+
+
+    public static void iniciar() {
+        double[] sismos = new double[168];
+        menu(sismos);
+    }
+
+
+    public static void leerSismos(double[] sismos) {
+        for (int i = 0; i < sismos.length; i++) {
+            System.out.println(sismos[i]);
+        }
     }
 }
